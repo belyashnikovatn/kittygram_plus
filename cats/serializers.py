9ec70d17a153b8484f4b321992e2741dd230b302
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-06-26 16:24:49 +0500

    add choices

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,18 +1,33 @@
 from rest_framework import serializers
 import datetime as dt
+import webcolors
 from .models import Cat, Owner, Achievement, AchievementCat
 
 
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
 
     class Meta:
         model = Cat
--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 from .models import Cat, Owner
@@ -7,6 +9,12 @@ from .serializers import CatSerializer, OwnerSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
