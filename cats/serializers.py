0cd390b96a3f0c27fd8ec7d096eb8247c76ce117
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-06-26 12:37:17 +0500

    cahnge create of CatSerializer

--- cats/serializers.py
+++ cats/serializers.py
@@ -11,13 +11,16 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 
 class CatSerializer(serializers.ModelSerializer):
-    achievements = AchievementSerializer(many=True)
+    achievements = AchievementSerializer(many=True, required=False)
 
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
 
     def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
         achievements = validated_data.pop('achievements')
         cat = Cat.objects.create(**validated_data)
 
